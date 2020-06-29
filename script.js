7818317f84ad4d001897eb34937ba04387254f23
Jack_the_Pro101 <59264946+Jack-the-Pro101@users.noreply.github.com>
2020-06-29 13:40:12 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 function askName() {
 let userName = prompt('What is your name? ☺');
 
-document.getElementById('name').innerHTML = userName;
+document.getElementById('name').innerHTML = userName + '!';
 }
 
 //If change again
 function askNameChange() {
     let changedName = prompt('What is your name again? ☺');
-    document.getElementById('name').innerHTML = changedName;
+    document.getElementById('name').innerHTML = changedName + '!';
 }
