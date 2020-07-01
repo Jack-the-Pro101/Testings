52ce62abcdafdfc56c4953538c06a0f69f55074c
Jack_the_Pro101 <59264946+Jack-the-Pro101@users.noreply.github.com>
2020-07-01 16:40:21 -0400

    Calc adding not working correctly
    Adding is currently changing the numbers into strings

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//Initial name change
+//Initial name change ---------------
 function askName() {
 let userName = prompt('What is your name? ☺');
 if (!userName) {
@@ -10,7 +10,7 @@ if (!userName) {
 document.getElementById('name').innerHTML = userName + '!';
 }
 
-//Counter
+//Counter ---------------------------
 let currentNumber;
 
 currentNumber = 0;
@@ -35,6 +35,70 @@ function numberDivi() {
     currentNumber = currentNumber/2;
     document.getElementById('counter').innerHTML = currentNumber;
 }
+
+//Calculator -----------------------
+
+let varCalcInput1;
+let varCalcInput2;
+
+function calcInput1() {
+    let input1;
+    input1 = prompt('Type desired number');
+    window.finalInput1 = input1;
+
+    document.getElementById('calcNumber1').innerHTML = input1;
+}
+function calcInput2() {
+    let input2;
+    input2 = prompt('Type desired number');
+    window.finalInput2 = input2;
+
+    document.getElementById('calcNumber2').innerHTML = input2;
+}
+function calcAdd() {
+    let result;
+    parseInt(finalInput1);
+    parseInt(finalInput2);
+
+    result = finalInput1+finalInput2;
+
+    document.getElementById('calcResult').innerHTML = result;               //let isnum = /^\d+$/.test(value);
+}
+function calcSub() {
+    let result;
+    Number(finalInput1);
+    Number(finalInput2);
+
+    result = finalInput1-finalInput2;
+
+    document.getElementById('calcResult').innerHTML = result;
+}
+function calcMul() {
+    let result;
+    Number(finalInput1);
+    Number(finalInput2);
+
+    result = finalInput1*finalInput2;
+
+    document.getElementById('calcResult').innerHTML = result;
+}
+function calcDiv() {
+    let result;
+    Number(finalInput1);
+    Number(finalInput2);
+
+    result = finalInput1/finalInput2;
+
+    document.getElementById('calcResult').innerHTML = result;
+}
+
+
+
+
+
+
+
+
 
 
 //Next time test with addEventListener
