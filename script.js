b0a004051a5edf62e1a8b4a01a91dda273efcdf9
Jack_the_Pro101 <59264946+Jack-the-Pro101@users.noreply.github.com>
2020-07-01 10:41:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ if (!userName) {
 
 document.getElementById('name').innerHTML = userName + '!';
 }
+
+//Counter
+let currentNumber;
+
+currentNumber = 0;
+
+function numberReset() {
+    currentNumber = 0;
+    document.getElementById('counter').innerHTML = currentNumber;
+}
+function numberIncr() {
+    currentNumber++;
+    document.getElementById('counter').innerHTML = currentNumber;
+}
+function numberDecr() {
+    currentNumber--;
+    document.getElementById('counter').innerHTML = currentNumber;
+}
+function numberMult() {
+    currentNumber = currentNumber*2;
+    document.getElementById('counter').innerHTML = currentNumber;
+}
+function numberDivi() {
+    currentNumber = currentNumber/2;
+    document.getElementById('counter').innerHTML = currentNumber;
+}
+
+
+//Next time test with addEventListener
